72009335d5972b2b7078e694bbafd19fb9a01e8d
Kyle Stevenson <kyle@kylestevenson.me>
2017-12-01 08:49:06 -0800

    Add validation of uploaded jars

--- routes/web.php
+++ routes/web.php
@@ -16,10 +16,22 @@ Route::get('jars/{jar}', function (Jar $jar) {
 });
 
 Route::post('jars', function (Request $request) {
+    $request->validate([
+        'jar' => [
+            'required',
+            'file',
+            'max:10240',
+            'mimetypes:application/java-archive,application/zip',
+            'mimes:jar',
+        ],
+    ]);
     $jar = new Jar(['user_id' => 1]);
     $jar->saveOrFail();
+
     $jar->addMediaFromRequest('jar')
         ->toMediaCollection();
+
     dispatch(new ExtractJarEntrypoints($jar->media->first()));
+
     return $jar->load('media');
 });
