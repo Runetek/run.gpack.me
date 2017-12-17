47658d6e6688daad2a629ac1e5dac1a15555d75e
Kyle Stevenson <kyle@kylestevenson.me>
2017-12-17 02:51:42 -0800

    Change column type for tokens

--- database/migrations/2017_12_17_083510_create_oauth_users_table.php
+++ database/migrations/2017_12_17_083510_create_oauth_users_table.php
@@ -17,7 +17,7 @@ class CreateOauthUsersTable extends Migration
             $table->increments('id');
             $table->string('name');
             $table->string('remember_token')->nullable();
-            $table->string('token');
+            $table->text('token');
             $table->timestamps();
         });
     }
