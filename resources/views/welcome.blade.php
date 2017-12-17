747c21e57b8600c9aeb32158305782106a8c169d
Kyle Stevenson <kyle@kylestevenson.me>
2017-12-17 02:48:38 -0800

    Update welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -66,28 +66,24 @@
     </head>
     <body>
         <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
+            @if (!Route::has('login'))
                 <div class="top-right links">
                     @auth
                         <a href="{{ url('/home') }}">Home</a>
                     @else
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
+                        <a href="{{ url('oauth') }}">Sign in with Runetek</a>
                     @endauth
                 </div>
             @endif
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Gpack Runner
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="https://sso.runetek.io">Runetek SSO</a>
+                    <a href="https://slack.runetek.io">RS-Hacking Slack</a>
                 </div>
             </div>
         </div>
